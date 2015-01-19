a96cb5f41cf7fcda2a4e1739343c2f41a232a286
Tom Underhill <tunderhill@gmail.com>
2015-01-18 21:56:07 -0800

    polish on the interface

--- app/controllers/happy_controller.rb
+++ app/controllers/happy_controller.rb
@@ -1,26 +1,26 @@
 class HappyController < ApplicationController
 
- 	@@message = ""
- 
-  # GET /happy
-  def index
-     @message = @@message
-  end
+	@@message = ""
 
-  # POST /happy/random_joke
-  def random_joke
-    jokes = Joke.all
-    joke = jokes.sample
-   	@@message = "#{joke.setup} - #{joke.punchline}"
-	redirect_to(happy_path)
-  end
+	# GET /happy
+	def index
+	 @message = @@message
+	end
 
-  # POST /happy/random_quote
-  def random_quote
-    quotes = Quote.all
-    quote = quotes.sample
-   	@@message = "#{quote.saying} - #{quote.source}"
-	redirect_to(happy_path)
-  end
+	# POST /happy/random_joke
+	def random_joke
+		jokes = Joke.all
+		joke = jokes.sample
+		@@message = "#{joke.setup} #{joke.punchline}"
+		redirect_to(happy_path)
+	end
+
+	# POST /happy/random_quote
+	def random_quote
+		quotes = Quote.all
+		quote = quotes.sample
+		@@message = "#{quote.saying} - #{quote.source}"
+		redirect_to(happy_path)
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,65 +1,67 @@
 Rails.application.routes.draw do
-  resources :quotes
+	resources :quotes
 
-  resources :jokes
+	resources :jokes
 
-  get 'happy', to: 'happy#index'
+	get 'happy', to: 'happy#index'
 
-  match 'happy/random_joke', to: 'happy#random_joke', via: [:post]
-  match 'happy/random_quote', to: 'happy#random_quote', via: [:post]
+	match 'happy/random_joke', to: 'happy#random_joke', via: [:post]
+	match 'happy/random_quote', to: 'happy#random_quote', via: [:post]
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+	root 'happy#index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+	# The priority is based upon order of creation: first created -> highest priority.
+	# See how all your routes lay out with "rake routes".
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+	# You can have the root of your site routed with "root"
+	# root 'welcome#index'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+	# Example of regular route:
+	#   get 'products/:id' => 'catalog#view'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+	# Example of named route that can be invoked with purchase_url(id: product.id)
+	#   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+	# Example resource route (maps HTTP verbs to controller actions automatically):
+	#   resources :products
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+	# Example resource route with options:
+	#   resources :products do
+	#     member do
+	#       get 'short'
+	#       post 'toggle'
+	#     end
+	#
+	#     collection do
+	#       get 'sold'
+	#     end
+	#   end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+	# Example resource route with sub-resources:
+	#   resources :products do
+	#     resources :comments, :sales
+	#     resource :seller
+	#   end
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+	# Example resource route with more complex sub-resources:
+	#   resources :products do
+	#     resources :comments
+	#     resources :sales do
+	#       get 'recent', on: :collection
+	#     end
+	#   end
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+	# Example resource route with concerns:
+	#   concern :toggleable do
+	#     post 'toggle'
+	#   end
+	#   resources :posts, concerns: :toggleable
+	#   resources :photos, concerns: :toggleable
+
+	# Example resource route within a namespace:
+	#   namespace :admin do
+	#     # Directs /admin/products/* to Admin::ProductsController
+	#     # (app/controllers/admin/products_controller.rb)
+	#     resources :products
+	#   end
 end
